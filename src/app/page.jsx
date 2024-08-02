f9fc1dd5b3b8afe7e4c10ac763d314885e641dd4
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-08-02 20:42:56 -0300

    Aula 02-08

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Aula Next JS</h1>
+      <h2>Exemplo h2</h2>
+    </div>
+  );
+}

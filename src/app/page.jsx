082089adb871afaa304bc5fa1c7a395118bb859d
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-08-06 20:40:56 -0300

    aula 06-08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Aula Next JS</h1>
+    <div className="containerGlobal">
+      <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
       <h2>Exemplo h2</h2>
+      <h3>Exemplo h3</h3>
+      <p>Texto parágrafo</p>
     </div>
   );
 }

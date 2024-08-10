8a1e540d94010f8a076950382ede9521426fbfc9
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-08-09 22:00:46 -0300

    aula 09-08

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,25 @@ export default function Home() {
       <h2>Exemplo h2</h2>
       <h3>Exemplo h3</h3>
       <p>Texto parágrafo</p>
+
+      <div className={styles.containerImagem}>
+        <Image
+          src="/pikachu.jpg"
+          width={500}
+          height={500}
+          alt="Pokémon Pikachu"
+          className={styles.imagemHomePage}
+        />
+
+        <Image
+          src="/pikachu.jpg"
+          width={50}
+          height={50}
+          alt="Pokémon Pikachu"
+          className={styles.imagemHomePage}
+        />
+      </div>
+
     </div>
   );
 }
